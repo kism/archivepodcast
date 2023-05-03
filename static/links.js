c5b2bd5c68871e40e0d6ce822590d6d5f34764ff
Kieran Gee <kieran.lost.the.game@gmail.com>
2023-05-03 23:12:33 +0800

    revert my fine javascript

--- static/links.js
+++
@@ -1,8 +0,0 @@
-//prepend the text field with the base url
-function replacetext(infield) {
-    console.log(infield);
-    infield.value = window.location + infield.value;
-}
-
-inputfields = document.querySelectorAll('input[type="text"]');
-inputfields.forEach(replacetext);
