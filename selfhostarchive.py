3fb8bc818d361756d31961fe9650d6e81bc964e1
Kieran Gee <kieran.lost.the.game@gmail.com>
2023-09-21 18:56:13 +0800

    smallest fix

--- selfhostarchive.py
+++ selfhostarchive.py
@@ -95,6 +95,7 @@ def static_from_root():
 
 @app.route('/favicon.ico')
 def favicon():
+    """Return the favicon"""
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
