eb05472279c5c41dbe0c7219795d5ac3c42baad6
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-12-02 15:18:50 +0800

    cleanup

--- archivepodcast/__init__.py
+++ archivepodcast/__init__.py
@@ -5,7 +5,7 @@ from flask import Flask
 from . import bp_archivepodcast, logger
 from .config import DEFAULT_LOGGING_CONFIG, ArchivePodcastConfig, print_config
 
-__version__ = "1.1.1"  # This is the version of the app, used in pyproject.toml, enforced in a test.
+__version__ = "1.1.2"  # This is the version of the app, used in pyproject.toml, enforced in a test.
 
 
 def create_app(test_config: dict | None = None, instance_path: str | None = None) -> Flask:
