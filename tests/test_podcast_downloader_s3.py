1d0f964fb01a7829b794b60f905fe973aaed6ecf
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-12-05 12:57:07 +0800

    fix s3 tests for downloader

--- tests/test_podcast_downloader_s3.py
+++ tests/test_podcast_downloader_s3.py
@@ -11,15 +11,18 @@ from archivepodcast.logger import TRACE_LEVEL_NUM
 FLASK_ROOT_PATH = os.getcwd()
 
 
-def test_init(get_test_config, tmp_path, caplog):
+def test_init(s3, get_test_config, tmp_path, caplog):
     """Test that the app can load config and the testing attribute is set."""
     config_file = "testing_true_valid_s3.toml"
     config = get_test_config(config_file)
 
+    bucket_name = config["app"]["s3"]["bucket"]
+    s3.create_bucket(Bucket=bucket_name)
+
     web_root = os.path.join(tmp_path, "web")
 
     with caplog.at_level(TRACE_LEVEL_NUM):
-        pd = PodcastDownloader(app_settings=config["app"], s3=None, web_root=web_root)
+        pd = PodcastDownloader(app_settings=config["app"], s3=s3, web_root=web_root)
 
     assert "PodcastDownloader settings (re)loaded" in caplog.text
     assert pd.s3_paths_cache == []
