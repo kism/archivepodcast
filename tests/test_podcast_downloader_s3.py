eb82feadd7d1a8460719f6fbc7c6ff58d57efca0
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-12-10 10:45:34 +0800

    more test coverage

--- tests/test_podcast_downloader_s3.py
+++ tests/test_podcast_downloader_s3.py
@@ -3,8 +3,6 @@
 import logging
 import os
 
-import pytest
-
 from archivepodcast.ap_downloader import PodcastDownloader
 from archivepodcast.logger import TRACE_LEVEL_NUM
 
@@ -106,7 +104,7 @@ def test_upload_asset_s3_no_client(apd, caplog):
 def test_upload_asset_s3_file_not_found(apd_aws, caplog):
     """Test upload failure when no s3 client."""
     with caplog.at_level(level=logging.ERROR, logger="archivepodcast.ap_downloader"):
-        apd_aws._upload_asset_s3("test_file_doesn't_exist.jpg", ".jpg")
+        apd_aws._upload_asset_s3("test_file_not_exist.jpg", ".jpg")
 
     assert "Could not upload to s3, the source file was not found" in caplog.text
 
@@ -123,6 +121,52 @@ def test_upload_asset_s3_unhandled_exception(apd_aws, monkeypatch, caplog):
     monkeypatch.setattr(apd_aws.s3, "upload_file", unhandled_exception)
 
     with caplog.at_level(level=logging.ERROR, logger="archivepodcast.ap_downloader"):
-        apd_aws._upload_asset_s3("test_file_doesn't_exist.jpg", ".jpg")
+        apd_aws._upload_asset_s3("test_file_not_exist.jpg", ".jpg")
+
+    assert "Unhandled s3 Error" in caplog.text
+
+def test_check_path_exists_s3(apd_aws, caplog):
+    """Test check path exists."""
+    apd_aws.s3.put_object( # Bucket is empty before this
+        Bucket=apd_aws.app_settings["s3"]["bucket"],
+        Key="content/test",
+        Body="Test File Found",
+        ContentType="text/html",
+    )
+
+    assert apd_aws._check_path_exists("content/test") is True
+    assert apd_aws._check_path_exists("/content/test") is True
+
+    with caplog.at_level(level=logging.DEBUG, logger="archivepodcast.ap_downloader"):
+        assert apd_aws._check_path_exists("content/test/not_exist") is False
+
+    assert "File: content/test/not_exist does not exist" in caplog.text
+
+def test_check_path_exists_s3_client_error(apd_aws, monkeypatch, caplog):
+    """Test non-404 s3 client error handling."""
+    from botocore.exceptions import ClientError
+
+    def client_error_not_404(*args, **kwargs) -> None:
+        raise ClientError({"Error": {"Code": "LimitExceededException"}}, "LimitExceededException")
+
+    monkeypatch.setattr(apd_aws.s3, "head_object", client_error_not_404)
+
+    with caplog.at_level(level=logging.ERROR, logger="archivepodcast.ap_downloader"):
+        assert apd_aws._check_path_exists("content/test") is False
+
+    assert "s3 check file exists errored out?" in caplog.text
+
+def test_check_path_exists_s3_unhandled_exception(apd_aws, monkeypatch, caplog):
+    """Test unhandled exception handling."""
+    class FakeExceptionError(Exception):
+        pass
+
+    def unhandled_exception(*args, **kwargs) -> None:
+        raise FakeExceptionError
+
+    monkeypatch.setattr(apd_aws.s3, "head_object", unhandled_exception)
+
+    with caplog.at_level(level=logging.ERROR, logger="archivepodcast.ap_downloader"):
+        assert apd_aws._check_path_exists("content/test") is False
 
     assert "Unhandled s3 Error" in caplog.text
