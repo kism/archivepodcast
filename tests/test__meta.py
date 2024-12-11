84813c8f47021139d4411e306389a7be02fa1207
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-12-11 09:47:14 +0800

    test wav file generation is real

--- tests/test__meta.py
+++ tests/test__meta.py
@@ -1,5 +1,9 @@
 """Test versioning."""
 
+import os
+
+import magic
+import pytest
 import tomlkit
 
 import archivepodcast
@@ -10,3 +14,12 @@ def test_version():
     with open("pyproject.toml", "rb") as f:
         pyproject_toml = tomlkit.load(f)
     assert pyproject_toml["project"]["version"] == archivepodcast.__version__
+
+def test_wav_generation(tmp_path):
+    """Test WAV file generation."""
+    wav_path = os.path.join(tmp_path, "test.wav")
+
+    with open(wav_path, "wb") as f:
+        f.write(pytest.TEST_WAV_FILE)
+
+    assert magic.from_file(wav_path, mime=True) == "audio/x-wav"
