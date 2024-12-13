238f51de4340b91516d71fd9af837b52b1593c15
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-12-13 13:11:25 +0800

    better tests

--- tests/test__meta.py
+++ tests/test__meta.py
@@ -9,11 +9,30 @@ import tomlkit
 import archivepodcast
 
 
-def test_version():
+def test_version_pyproject():
     """Test version variable."""
     with open("pyproject.toml", "rb") as f:
         pyproject_toml = tomlkit.load(f)
-    assert pyproject_toml["project"]["version"] == archivepodcast.__version__
+    assert (
+        pyproject_toml["project"]["version"] == archivepodcast.__version__
+    ), "Ensure pyproject.toml version matches __init__.py"
+
+
+def test_version_lock():
+    """Test version in lock file."""
+    with open("uv.lock") as f:
+        uv_lock = tomlkit.load(f)
+
+    found_uv_lock_version = False
+
+    for package in uv_lock["package"]:
+        if package["name"] == "archivepodcast":
+            assert (
+                package["version"] == archivepodcast.__version__
+            ), "uv.lock package version not in-sync, run: uv sync --upgrade"
+            found_uv_lock_version = True
+
+    assert found_uv_lock_version, "Ensure archivepodcast is in uv.lock"
 
 
 def test_wav_generation(tmp_path):
