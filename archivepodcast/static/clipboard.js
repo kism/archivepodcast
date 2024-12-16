7c34e94db04b35d5645f537156e723a935ce9531
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-12-16 16:04:06 +0800

    better cspell

--- archivepodcast/static/clipboard.js
+++ archivepodcast/static/clipboard.js
@@ -1,6 +1,5 @@
 function grab_to_clipboard(button_name) {
   console.log(`User clicked: ${button_name}`);
-  // Get the text field
   const copyText = document.getElementById(button_name);
 
   // Select the text field
@@ -9,9 +8,6 @@ function grab_to_clipboard(button_name) {
 
   // Copy the text inside the text field
   navigator.clipboard.writeText(copyText.value);
-
-  // Alert the copied text
-  console.log(`Copied the text: ${copyText.value} to clipboard`);
 
   document.getElementById(`${button_name}_button`).innerHTML = "Copied!";
   setTimeout(reset_text, 2000, button_name);
