2ae534f92d89c75dbbc58dd816e5626e6923250f
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-12-17 15:40:17 +0800

    coverage setup

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    test: {
+      include: ["tests/*.js"],
+      coverage: {
+        provider: 'v8',
+        include: ['archivepodcast/static/*.js'],
+        reportsDirectory: 'htmlcov_js',
+      },
+    },
+  })
