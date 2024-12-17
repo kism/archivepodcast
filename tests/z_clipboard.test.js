d01bf97203877aef31bba5729f54a8f79d3dc5e0
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-12-17 15:27:50 +0800

    first js testing

---
+++ tests/z_clipboard.test.js
@@ -0,0 +1,28 @@
+// @vitest-environment happy-dom
+
+import { expect, test } from 'vitest'
+
+import { grabToClipboard } from '../archivepodcast/static/clipboard.js'
+
+test('grabToClipboard with non-existent element', () => {
+    const button_name = "non_existent_button"
+    expect(() => grabToClipboard(button_name)).toThrow()
+})
+
+test('grabToClipboard with value', () => {
+    const rss_url = "http://localhost:5000/rss/test"
+    const button_name = "button_name"
+    const copyText = document.createElement('input')
+    copyText.id = button_name
+    copyText.value = rss_url
+    document.body.appendChild(copyText)
+
+    const button = document.createElement('button')
+    button.id = `${button_name}_button`
+    document.body.appendChild(button)
+
+    grabToClipboard(button_name)
+
+    expect(copyText.value).toBe(rss_url)
+    expect(document.getElementById(`${button_name}_button`).innerHTML).toBe('Copied!')
+})
