55ab2e7340980519b95dd1b132ccde697e83d947
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-12-19 15:33:52 +0800

    fix paths

--- tests/test_home.py
+++ tests/test_home.py
@@ -57,11 +57,11 @@ def test_guide_exists(client, apa):
 def test_fonts_exist(client):
     """TEST: /static/fonts/... loads."""
     font_list = [
-        "/static/fonts/fira-code-v12-latin-500.woff2",
-        "/static/fonts/fira-code-v12-latin-600.woff2",
-        "/static/fonts/fira-code-v12-latin-700.woff2",
-        "/static/fonts/noto-sand-display-latin-500.woff2",
-        "/static/fonts/noto-sand-display-latin-500italic.woff2",
+        "static/fonts/fira-code-v12-latin-500.woff2",
+        "static/fonts/fira-code-v12-latin-600.woff2",
+        "static/fonts/fira-code-v12-latin-700.woff2",
+        "static/fonts/noto-sand-display-latin-500.woff2",
+        "static/fonts/noto-sand-display-latin-500italic.woff2",
     ]
     for font in font_list:
         response = client.get(font)
