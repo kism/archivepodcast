9449556e4b9101b3c17465f8d9c2dc81903d16e6
Kieran Gee <kieran.lost.the.game@gmail.com>
2025-02-07 13:21:56 +0800

    wip docs

---
+++ docs/conf.py
@@ -0,0 +1,23 @@
+# -- Project information -----------------------------------------------------
+
+project = "Archive Podcast"
+copyright = "2025, Kieran Gee"
+author = "Kieran Gee"
+
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "myst_parser",
+]
+
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
---
+++ docs/index.rst
@@ -0,0 +1,1 @@
+[deoloy_docker]
