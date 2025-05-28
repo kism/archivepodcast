4f04e91b767496c1827aee9c600d8e0a5b6ff1b5
Kieran Gee <kieran.lost.the.game@gmail.com>
2025-05-28 13:44:46 +0800

    wip

--- tests/test_podcast_archiver.py
+++ tests/test_podcast_archiver.py
@@ -206,3 +206,32 @@ def test_grab_podcasts_unhandled_exception_rss(
         apa.grab_podcasts()
 
     assert "Unable to download podcast, something is wrong" in caplog.text
+
+def test_grab_podcasts_no_episodes(
+    apa,
+    caplog,
+    mock_get_podcast_source_rss,
+    mock_podcast_source_images,
+    mock_podcast_source_mp3,
+):
+    """Test grabbing podcasts."""
+    mock_get_podcast_source_rss("test_valid_no_episodes.rss")
+
+    apa.podcast_list[0]["live"] = True
+
+    rss_path = Path(apa.instance_path) / "web" / "rss" / "test"
+    rss_path.parent.mkdir(parents=True, exist_ok=True)
+    rss_path.write_text(pytest.DUMMY_RSS_STR)
+
+    with caplog.at_level(level=logging.DEBUG, logger="archivepodcast.ap_archiver"):
+        apa.grab_podcasts()
+
+    assert "Processing podcast to archive: PyTest Podcast [Archive]" not in caplog.text
+    assert "Cannot find rss feed file" not in caplog.text
+    assert "Unable to host podcast, something is wrong" not in caplog.text
+
+    assert "No response, loading rss from file" not in caplog.text  # This shouldn't happen
+
+    get_rss = str(apa.get_rss_feed("test"), "utf-8")
+
+    assert get_rss == pytest.DUMMY_RSS_STR
