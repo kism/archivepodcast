9bcb471208d74209020f52ea5403ec1839be3671
Kieran Gee <kieran.lost.the.game@gmail.com>
2025-10-08 11:49:57 +0800

    bump version

--- archivepodcast/version.py
+++ archivepodcast/version.py
@@ -1,3 +1,3 @@
 """Version information."""
 
-__version__ = "1.4.20.dev1"  # This is the version of the app, used in pyproject.toml, enforced in a test.
+__version__ = "1.4.20"  # This is the version of the app, used in pyproject.toml, enforced in a test.
