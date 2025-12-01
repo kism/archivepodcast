b67582d24c00a3fe591bc2e07bed0115b8951cb5
Kieran Gee <kieran.lost.the.game@gmail.com>
2025-12-01 19:05:16 +0800

    appease ty

--- archivepodcast/utils/time.py
+++ archivepodcast/utils/time.py
@@ -13,7 +13,7 @@ def warn_if_too_long(msg: str, time_diff: timedelta | float, *, large_file: bool
     """Prints a log message if the time difference is too long."""
     threshold = WARN_THRESHOLD * (10 if large_file else 1)
 
-    if isinstance(time_diff, float):
-        time_diff = timedelta(seconds=time_diff)
-    if time_diff > timedelta(seconds=threshold):
-        logger.warning("Operation: %s took longer than expected: %ss", msg, int(time_diff.total_seconds()))
+    time_delta = timedelta(seconds=time_diff) if (isinstance(time_diff, (float, int))) else time_diff
+
+    if time_delta >= timedelta(seconds=threshold):
+        logger.warning("Operation: %s took longer than expected: %ss", msg, int(time_delta.total_seconds()))
