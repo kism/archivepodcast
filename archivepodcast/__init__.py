4f796ec029ce9af0557efd09f1eb953ae299ec20
Kieran Gee <kieran.lost.the.game@gmail.com>
2025-12-04 17:18:26 +0800

    bump version

--- archivepodcast/__init__.py
+++ archivepodcast/__init__.py
@@ -17,6 +17,9 @@ from .instances.profiler import event_times
 from .utils import logger as ap_logger
 from .utils.log_messages import log_intro
 from .utils.profiler import get_event_times_str
+from .version import __version__
+
+__all__ = ["__version__", "create_app", "run_ap_adhoc"]
 
 # Don't if we are in lambda
 if not ap_logger.force_simple_logger():
--- archivepodcast/version.py
+++ archivepodcast/version.py
@@ -1,3 +1,3 @@
 """Version information."""
 
-__version__ = "1.5.3"  # This is the version of the app, used in pyproject.toml, enforced in a test.
+__version__ = "1.5.4"  # This is the version of the app, used in pyproject.toml, enforced in a test.
