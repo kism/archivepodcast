24f239c56bd585953eab25d9f9e9e4ade3eb1654
Kieran Gee <kieran.lost.the.game@gmail.com>
2025-12-05 19:14:53 +0800

    fix docker ci

--- scripts/run_docker_main_webserver.sh
+++ scripts/run_docker_main_webserver.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 docker build -f docker/_dep_ffmpeg_lambda.Dockerfile -t archivepodcast:ffmpeg-bookworm .
 
-docker build -f docker/main.Dockerfile -t archivepodcast .
+docker build -f docker/main.Dockerfile -t archivepodcast:latest .
 
 docker run \
     --rm \
