32a32d2760a8c00eaf67c01f13ec4001b28dff04
Kieran Gee <kieran.lost.the.game@gmail.com>
2026-04-28 08:13:38 +0800

    fix lambda

--- archivepodcast/version.py
+++ archivepodcast/version.py
@@ -1,3 +1,3 @@
 """Version information."""
 
-__version__ = "1.5.12"  # This is the version of the app, used in pyproject.toml, enforced in a test.
+__version__ = "1.5.13.dev1"  # This is the version of the app, used in pyproject.toml, enforced in a test.
