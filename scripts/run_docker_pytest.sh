1fbb7c01611bfae6629ba86e2899d27197f1fb91
Kieran Gee <kieran.lost.the.game@gmail.com>
2026-09-14 10:29:07 +0800

    fix ci

--- scripts/run_docker_pytest.sh
+++ scripts/run_docker_pytest.sh
@@ -2,17 +2,27 @@
 
 set -euo pipefail
 
-# AL
+MAGENTA='\033[0;35m'
+NC='\033[0m' # No Color
+
+echo_magenta() {
+    echo
+    echo -e "--- ${MAGENTA}$1${NC} ---"
+}
+
+echo_magenta "Building ffmpeg image"
 docker build -f docker/_dep_ffmpeg.Dockerfile -t archivepodcast:ffmpeg .
 docker build -f docker/main_al.Dockerfile -t archivepodcast:al2023 .
 
-# DEBIAN
+echo_magenta "Building main image"
 docker build -f docker/_dep_ffmpeg.Dockerfile -t archivepodcast:ffmpeg .
 docker build -f docker/main.Dockerfile -t archivepodcast:latest .
 
-# PYTEST
+echo_magenta "Building pytest image"
 docker build -f docker/pytest.Dockerfile -t archivepodcast:pytest .
+
+echo_magenta "Running pytest image"
 docker run --rm archivepodcast:pytest
 
-# SELF TEST AL
+echo_magenta "Running self test"
 docker run --rm --env AP_SELF_TEST=true archivepodcast:al2023 sh -c "mkdir -p /tmp/instance && archivepodcast --instance-path /tmp/instance"
